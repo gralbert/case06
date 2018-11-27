80d6a1492936e9407a09e6ff129909ed9fd6acf0
gralbert <albertgralin@gmail.com>
2018-11-27 10:39:40 +0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,22 @@
+def get_color_choice():
+    # TODO
+
+
+def get_num_hexagons():
+    # TODO
+
+
+def draw_hexagon(x, y, side_len, color):
+    # TODO
+
+
+def tiling(number, color1, color2):
+    # TODO
+
+
+def main():
+    # TODO
+
+
+if __name__ == '__main__':
+    main()
